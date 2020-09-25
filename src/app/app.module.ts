d1c256c8b7e999c1ed637234f3d9151eddbb35ad
amillar5503 <amillar5503@gmail.com>
2020-09-25 15:55:10 -0700

    7.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,13 @@ import { ProductDescriptionComponent } from './product-description/product-descr
 import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { Z_FULL_FLUSH } from 'zlib';
 
-@Routes({
-
-})
+const appRoutes: Routes = [
+  {path: 'products', component: ProductListComponent },
+  {path: 'product/:id', component: ProductPageComponent},
+  {path: '', redirectTo: 'products', pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
